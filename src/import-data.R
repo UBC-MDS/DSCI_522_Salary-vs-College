1353f78c30ac04962dd7d9b11174e86b23a843bb
lyyu <lyyu.utoronto@gmail.com>
2018-11-17 01:30:48 -0800

    Update the data cleaning script
    Change the character type with dollar sign to plain numeric data

--- src/import-data.R
+++ src/import-data.R
@@ -1,9 +1,18 @@
+# import-data.R
+# Group: lab2-11
+
+# This script read the original data as .csv files
+
+# Usage: Rscript import-data.R
+
 library(tidyverse)
 
 salary_by_degree <- read_csv("data/degrees-that-pay-back.csv")
 salary_by_type <- read_csv("data/salaries-by-college-type.csv")
 salary_by_region <- read_csv("data/salaries-by-region.csv")
 
+salary_by_degree
 
 # NOTE: Right now, salaries are chars. Need to convert to numeric values. 
-salary_by_degree
+
+
