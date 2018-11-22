7829cf649625367a84982cfea7d33a2f43aaafb2
lyyu <lyyu.utoronto@gmail.com>
2018-11-21 20:39:46 -0800

    Join the 'region' and 'school type' dataset
    In order to perform 2-way ANOVA, we join the tables from "clean_salary_by_region.csv" and "clean_salary_by_type.csv"

---
+++ src/Join_region_and_type.R
@@ -0,0 +1,26 @@
+# Join_region_and_type.R
+# Group: lab2-11
+
+# Because we are interested in a 2-factor ANOVA test (by region and school type), this script is used to perform inner join for the two datasets:
+# 1. data/clean_salary_by_region.csv
+# 2. data/clean_salary_by_type.csv 
+# And create a third .csv file for storing the joined dataframes.
+# Since the goal for this script is very direct, the script will take no argument from the command bash
+
+# Usage: Rscript Join_region_and_type.R
+
+# import libraries
+library(tidyverse)
+
+# define and read the .csv files
+input_file_path_1 <- 'data/clean_salary_by_region.csv'
+input_file_path_2 <- 'data/clean_salary_by_type.csv'
+df_r <- read_csv(input_file_path_1)
+df_t <- read_csv(input_file_path_2)
+
+# perform the inner join
+df_t_select <- df_t %>% select(School_Name, School_Type)
+joined_table <- inner_join(df_r, df_t_select, by = "School_Name") %>% select(School_Name, Region, School_Type, everything())
+
+# Export the new .csv file
+write.csv(joined_table, file = "data/clean_salary_joined_region_and_type.csv", row.names = FALSE)
