b11c8a7dd8497d43fc547e78eefc9795bf379570
lyyu <lyyu.utoronto@gmail.com>
2018-11-22 12:42:13 -0800

    Based on the renewed structure of our repo, rewrite the table join script

--- src/Join_region_and_type.R
+++ src/Join_region_and_type.R
@@ -2,8 +2,8 @@
 # Group: lab2-11
 
 # Because we are interested in a 2-factor ANOVA test (by region and school type), this script is used to perform inner join for the two datasets:
-# 1. data/clean_salary_by_region.csv
-# 2. data/clean_salary_by_type.csv 
+# 1. data/clean_data/clean_salary_by_region.csv
+# 2. data/clean_data/clean_salary_by_type.csv 
 # And create a third .csv file for storing the joined dataframes.
 # Since the goal for this script is very direct, the script will take no argument from the command bash
 
@@ -13,14 +13,16 @@
 library(tidyverse)
 
 # define and read the .csv files
-input_file_path_1 <- 'data/clean_salary_by_region.csv'
-input_file_path_2 <- 'data/clean_salary_by_type.csv'
-df_r <- read_csv(input_file_path_1)
-df_t <- read_csv(input_file_path_2)
+args <- commandArgs(trailingOnly = TRUE)
+input_file_path_1 <- args[1] # data/clean_data/clean_salary_by_type.csv
+input_file_path_2 <- args[2] # data/clean_data/clean_salary_by_region.csv
+
+df_t <- read_csv(input_file_path_1)
+df_r <- read_csv(input_file_path_2)
 
 # perform the inner join
 df_t_select <- df_t %>% select(School_Name, School_Type)
 joined_table <- inner_join(df_r, df_t_select, by = "School_Name") %>% select(School_Name, Region, School_Type, everything())
 
 # Export the new .csv file
-write.csv(joined_table, file = "data/clean_salary_joined_region_and_type.csv", row.names = FALSE)
+write.csv(joined_table, file = "data/clean_data/joined_region_and_type.csv", row.names = FALSE)
